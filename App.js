2c180b9c381f68ddf3ab82731cac6afe5fb6a010
taystein435 <taystein435@gmail.com>
2022-12-06 12:00:08 +0100

    Second commit

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Ecstasy main</Text>
+      <Text>Ecstasy main</Text>
       <StatusBar style="auto" />
     </View>
   );
